4789300c7d67a14ee66c27b4b2b1384525dee9a1
Nick Tazelaar <nick@nicktazelaar.com>
2021-06-28 23:43:22 +0200

    Added implementation and migrations

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.5.16;
 
 contract TodoList {
-    uint taskCount = 0;
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Send an email to nick@nicktazelaar.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
